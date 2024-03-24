0ee2804f5b830a5636c28548e97f7e202cf6e2be
Charanjit Singh <charanjit.singh@on-running.com>
2024-03-24 17:27:46 +0100

    chore: rudimentary deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-rsync -raPhz ../build/vidhi/ lab:/var/www/vidhi-new/
+rsync -raPhz ./build/vidhi/ lab:/var/www/vidhi-new/
 ssh lab <<EOT
 mv /var/www/vidhi /var/www/vidhi-old;
 mv /var/www/vidhi-new /var/www/vidhi;
